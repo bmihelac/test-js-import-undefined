13516ba7ab3fc7629bdad23150462882e5a9183f
Bojan Mihelac <bmihelac@mihelac.org>
2015-11-25 10:08:11 +0100

    initial import

---
+++ actionTypes.js
@@ -0,0 +1,1 @@
+export const UPDATE_REQUEST = 'UPDATE_REQUEST';
---
+++ reducer.js
@@ -0,0 +1,15 @@
+import * as types from './actionTypes';
+
+export default function pouchdbReducer(state = {}, action) {
+  switch (action.type) {
+    case types.UPDDATE_REQUEST:
+      // there is a typo above and it evaluates to `undefined`
+      // this code would never be reached - how to make it an error
+      return Object.assign({}, state, {updated: true});
+    default:
+      return state;
+  }
+}
+
+const state = pouchdbReducer({}, {type: types.UPDATE_REQUEST});
+console.log((state.updated == true) + ' should be true');
